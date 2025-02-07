799ca67bb59d22698ebf0547cfb7f8df1f8b4973
SAMahlangu <114006153+SAMahlangu@users.noreply.github.com>
2025-02-07 18:10:52 +0200

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,58 +1,24 @@
-import React, { useState } from 'react';
-import './Header.css'; // Import the CSS file
-import { 
-  BsFillBellFill, 
-  BsFillEnvelopeFill, 
-  BsPersonCircle, 
-  BsSearch 
-} from 'react-icons/bs';
+import React from 'react'
+import 
+ {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+ from 'react-icons/bs'
 
-function Header() {
-  const [showSearch, setShowSearch] = useState(false);
-
-  const handleNotifications = () => {
-    alert("You have a new Notification");
-  };
-
-  const handleMessages = () => {
-    alert("You have a new Message");
-  };
-
-  const handleProfile = () => {
-    alert("Redirecting to your profile...");
-  };
-
+function Header({OpenSidebar}) {
   return (
-    <header className="header">
-      <div className="header-right">
-        {showSearch && (
-          <input 
-            type="text" 
-            className="search-bar" 
-            placeholder="Search..." 
-            autoFocus 
-            onBlur={() => setShowSearch(false)} // Close search bar when it loses focus
-          />
-        )}
-        <BsSearch 
-          className="icon"  
-          onClick={() => setShowSearch(true)} 
-        />
-        <BsFillBellFill 
-          className="icon" 
-          onClick={handleNotifications} 
-        />
-        <BsFillEnvelopeFill 
-          className="icon" 
-          onClick={handleMessages} 
-        />
-        <BsPersonCircle 
-          className="icon" 
-          onClick={handleProfile} 
-        />
-      </div>
+    <header className='header'>
+        <div className='menu-icon'>
+            <BsJustify className='icon' onClick={OpenSidebar}/>
+        </div>
+        <div className='header-left'>
+            <BsSearch  className='icon'/>
+        </div>
+        <div className='header-right'>
+            <BsFillBellFill className='icon'/>
+            <BsFillEnvelopeFill className='icon'/>
+            <BsPersonCircle className='icon'/>
+        </div>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
